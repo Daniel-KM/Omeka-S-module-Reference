b17a0fcbbb8b4736ee01b7d3137243599cbe1ab9
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-07-29 00:00:00 +0000

    Fixed injection of module templates.

--- src/Service/ThemeManagerFactory.php
+++ src/Service/ThemeManagerFactory.php
@@ -79,16 +79,19 @@ class ThemeManagerFactory implements FactoryInterface
 
             $theme->setState(ThemeManager::STATE_ACTIVE);
 
-            // Inject module templates.
-            $configSpec['block_templates'] = empty($configSpec['block_templates'])
-                ? $moduleBlockTemplates
-                : array_merge_recursive($configSpec['block_templates'], $moduleBlockTemplates);
+            // Inject module templates, with priority to theme templates.
+            // Take care of merge with duplicate template keys.
             if (count($modulePageTemplates)) {
                 $configSpec['page_templates'] = empty($configSpec['page_templates'])
                     ? $modulePageTemplates
-                    : array_merge_recursive($configSpec['page_templates'], $modulePageTemplates);
+                    : array_replace($modulePageTemplates, $configSpec['page_templates']);
             }
-            $theme->setConfigSpec($configSpec);
+            $configSpec['block_templates'] = empty($configSpec['block_templates'])
+                ? $moduleBlockTemplates
+                // Array_merge_recursive() converts duplicate keys to array.
+                // Array_map() removes keys.
+                : array_replace_recursive($moduleBlockTemplates, $configSpec['block_templates']);
+                $theme->setConfigSpec($configSpec);
         }
 
         // Note that, unlike the ModuleManagerFactory, this does not register
