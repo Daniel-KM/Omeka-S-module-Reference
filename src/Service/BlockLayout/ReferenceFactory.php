1b2209bf77bb1278d1bfbcd0f18f5cf1bf904811
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-07-29 00:00:00 +0000

    Updated Common version.

--- src/Service/BlockLayout/ReferenceFactory.php
+++ src/Service/BlockLayout/ReferenceFactory.php
@@ -16,7 +16,7 @@ class ReferenceFactory implements FactoryInterface
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
         return new Reference(
-            $services->get('EasyMeta')
+            $services->get('Common\EasyMeta')
         );
     }
 }
