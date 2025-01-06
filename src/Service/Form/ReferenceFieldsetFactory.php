d470041b51bee0718ca44f1974840e012eef917b
Daniel Berthereau <Daniel.git@Berthereau.net>
2025-01-06 00:00:00 +0000

    Added a check for module Advanced Search.

--- src/Service/Form/ReferenceFieldsetFactory.php
+++ src/Service/Form/ReferenceFieldsetFactory.php
@@ -11,25 +11,37 @@ class ReferenceFieldsetFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        /** @see \AdvancedSearch\Service\Form\SearchingFormFieldsetFactory */
+        /**
+         * @see \AdvancedSearch\Service\Form\SearchingFormFieldsetFactory
+         * Adapted:
+         * @see \BlockPlus\Service\Form\SearchFormFieldsetFactory
+         * @see \Reference\Service\Form\ReferenceFieldsetFactory
+         */
 
         $configs = [];
 
-        $siteSettings = $services->get('Omeka\Settings\Site');
-        $available = $siteSettings->get('advancedsearch_configs', []);
+        if ($services->get('Omeka\ApiAdapterManager')->has('search_configs')) {
+            $siteSettings = $services->get('Omeka\Settings\Site');
+            $availableSearchConfigs = $siteSettings->get('advancedsearch_configs', []);
 
-        /** @var \AdvancedSearch\Api\Representation\SearchConfigRepresentation[] $searchConfigs */
-        $api = $services->get('Omeka\ApiManager');
-        $searchConfigs = $api->search('search_configs', ['id' => $available])->getContent();
+            /** @var \AdvancedSearch\Api\Representation\SearchConfigRepresentation[] $searchConfigs */
+            $api = $services->get('Omeka\ApiManager');
+            $searchConfigs = $api->search('search_configs', ['id' => $availableSearchConfigs])->getContent();
 
-        foreach ($searchConfigs as $searchConfig) {
-            $configs[$searchConfig->id()] = sprintf('%s (/%s)', $searchConfig->name(), $searchConfig->slug());
-        }
+            foreach ($searchConfigs as $searchConfig) {
+                $configs[$searchConfig->id()] = sprintf(
+                    '%s (/%s)',
+                    $searchConfig->name(),
+                    // Support of old version of module AdvancedSearch.
+                    method_exists($searchConfig, 'path') ? $searchConfig->path() : $searchConfig->slug()
+                );
+            }
 
-        // Set the main search config first and as default.
-        $default = $siteSettings->get('advancedsearch_main_config') ?: reset($available);
-        if (isset($configs[$default])) {
-            $configs = [$default => $configs[$default]] + $configs;
+            // Set the main search config first and as default.
+            $default = $siteSettings->get('advancedsearch_main_config') ?: reset($availableSearchConfigs);
+            if (isset($configs[$default])) {
+                $configs = [$default => $configs[$default]] + $configs;
+            }
         }
 
         $classes = [
